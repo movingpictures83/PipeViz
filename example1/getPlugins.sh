5004cf73e14c94c4058a9f5e8ce4717b40908c05
movingpictures83 <movingpictures83@gmail.com>
2020-09-15 21:20:40 -0400

    Mouse

---
+++ example1/getPlugins.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+unzip silva.bacteria.fasta.zip
+git clone https://github.com/movingpictures83/ATria.git ../../plugins/ATria
+git clone https://github.com/movingpictures83/CountTableProcessing.git ../../plugins/CountTableProcessing
+git clone https://github.com/movingpictures83/CSV2GML.git ../../plugins/CSV2GML
+git clone https://github.com/movingpictures83/CSVNormalize.git ../../plugins/CSVNormalize
+git clone https://github.com/movingpictures83/CSVPad.git ../../plugins/CSVPad
+git clone https://github.com/movingpictures83/GPUATria.git ../../plugins/GPUATria
+git clone https://github.com/movingpictures83/Mothur.git ../../plugins/Mothur
+git clone https://github.com/movingpictures83/Spearman.git ../../plugins/Spearman
+
